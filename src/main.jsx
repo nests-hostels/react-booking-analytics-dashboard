036280286a2c9efefb9bc8966cc761694e148da9
Artur Mamedov <arturmamedov1993@gmail.com>
2025-08-08 17:49:48 +0200

    feat: add HostelAnalytics component for data processing and visualization; update import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HostelAnalytics from './HostelAnalytics.jsx'
+import HostelAnalytics from './components/HostelAnalytics.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
